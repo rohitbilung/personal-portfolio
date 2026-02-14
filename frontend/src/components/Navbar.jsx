694a249d2b5592f591f6ba7952197dbaced6d068
Rohit <rohitbilung@gmail.com>
2026-02-14 14:22:54 +0530

    testing2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   const navLinks = [
     // { name: 'About', href: '#about' },
     // { name: 'Experience', href: '#experience' },
-    // { name: 'Projects', href: '#projects' },
+    { name: 'Projects', href: '#projects' },
     // { name: 'Contact', href: '#contact' },
   ];
 
